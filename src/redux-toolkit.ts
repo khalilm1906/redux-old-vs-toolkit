89349e9fd394d265e5000c6a49a9985ac3c121b3
Khalil Malik <Khalilm1906@gmail.com>
2020-03-12 00:18:51 -0500

    first commit

---
+++ src/redux-toolkit.ts
@@ -0,0 +1,103 @@
+import { configureStore, createSlice, PayloadAction, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { v1 as uuid } from 'uuid';
+import logger from 'redux-logger';
+
+import { Todo } from './type';
+
+const todosInitalState: Todo[] = [
+    {
+      id: uuid(),
+      desc: "Learn React",
+      isComplete: true
+    },
+    {
+      id: uuid(),
+      desc: "Learn Redux",
+      isComplete: true
+    },
+    {
+      id: uuid(),
+      desc: "Learn Redux-ToolKit",
+      isComplete: false
+    }
+];
+
+const todosSlice = createSlice({
+    name: 'todos',
+    initialState: todosInitalState,
+    reducers: {
+        create: {
+            prepare: ({ desc }: { desc: string }) => ({
+                payload: {
+                    id: uuid(),
+                    desc,
+                    isComplete: false
+                }
+            }),
+            reducer: (state, {payload}: PayloadAction<Todo>)  => {
+              state.push(payload);  
+            }
+        },
+        edit: (state, { payload }: PayloadAction<{ id: string; desc: string }>) => {
+            const todoToEdit = state.find(todo => todo.id === payload.id);
+            if (todoToEdit) {
+                todoToEdit.desc  = payload.desc;
+            }
+        },
+        toggle: (state, { payload }: PayloadAction<{id: string; isComplete: boolean}>) => {
+            const todoToToggle = state.find(todo => todo.id === payload.id);
+            if (todoToToggle) {
+                todoToToggle.isComplete = payload.isComplete;
+            }
+        },
+        remove: (state, { payload }: PayloadAction<{ id: string }>) => {
+            const index = state.findIndex(todo => todo.id === payload.id);
+            if (index !== -1) {  
+                state.splice(index, 1);
+            } 
+        }
+      }
+  })
+
+  const selectedTodoSlice  = createSlice({
+      name: 'selectedTodo',
+      initialState: null as string | null,
+      reducers: {
+          select: (state, {payload}: PayloadAction<{id: string}>) => payload.id
+      }
+  });
+
+  const counterSlice = createSlice({
+      name: 'counter',
+      initialState: 0,
+      reducers: {},
+      extraReducers: {
+          [todosSlice.actions.create.type]: state => state + 1,
+          [todosSlice.actions.edit.type]: state => state + 1,
+          [todosSlice.actions.remove.type]: state => state +1,
+          [todosSlice.actions.toggle.type]: state => state + 1
+      }
+  });
+export const {
+    create: createTodoActionCreator,
+    edit: editTodoActionCreator,
+    toggle: toggleTodoActionCreator,
+    remove: deleteTodoActionCreator,
+} = todosSlice.actions;
+
+export const {
+    select: selectTodoActionCreator
+} = selectedTodoSlice.actions;
+
+const reducer = {
+    todos: todosSlice.reducer,
+    selectedTodo: selectedTodoSlice.reducer,
+    counter: counterSlice.reducer
+};
+
+const middleware = [...getDefaultMiddleware(), logger];
+export default configureStore({
+    reducer,
+    middleware,
+    devTools: true // can set this dynamicallly with env vairable
+})
